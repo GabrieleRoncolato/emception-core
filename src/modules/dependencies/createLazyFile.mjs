5e9b166ac5c1a2514fabf25ab64e7a563bc4a285
GabrieleRoncolato <roncolatogabriele@gmail.com>
2024-02-06 18:46:46 +0100

    Fixing sync request by turning xhr into async

--- src/modules/dependencies/createLazyFile.mjs
+++ src/modules/dependencies/createLazyFile.mjs
@@ -9,27 +9,38 @@ export default function createLazyFile(FS, parent, name, datalength, url, canRea
     }
     LazyUint8Array.prototype.cacheLength = function LazyUint8Array_cacheLength() {
         // Function to get a range from the remote URL.
-        var doXHR = () => {
-            // TODO: Use mozResponseArrayBuffer, responseStream, etc. if available.
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', url, false);
+        var doXHR = async () => {
+            return new Promise((resolve, reject) => {
+                // TODO: Use mozResponseArrayBuffer, responseStream, etc. if available.
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url, true);
 
-            // Some hints to the browser that we want binary data.
-            xhr.responseType = 'arraybuffer';
-            if (xhr.overrideMimeType) {
-                xhr.overrideMimeType('text/plain; charset=x-user-defined');
-            }
+                // Some hints to the browser that we want binary data.
+                xhr.responseType = 'arraybuffer';
+                if (xhr.overrideMimeType) {
+                    xhr.overrideMimeType('text/plain; charset=x-user-defined');
+                }
 
-            xhr.send(null);
-            if (!(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304)) throw new Error("Couldn't load " + url + ". Status: " + xhr.status);
-            if (xhr.response !== undefined) {
-                return new Uint8Array(/** @type{Array<number>} */(xhr.response || []));
-            }
-            return intArrayFromString(xhr.responseText || '', true);
+                xhr.onload = () => {
+                    if (!(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304)) 
+                        throw new Error("Couldn't load " + url + ". Status: " + xhr.status);
+                    
+                    if (xhr.response !== undefined) {
+                        resolve(new Uint8Array(/** @type{Array<number>} */(xhr.response || [])));
+                    }
+
+                    resolve(intArrayFromString(xhr.responseText || '', true));
+                };
+
+                xhr.onerror = () => reject(new Error("Network error")); // Reject the promise on network errors
+                xhr.ontimeout = () => reject(new Error("Request timed out")); 
+
+                xhr.send(null);
+            });
         };
-        this.get = () => {
+        this.get = async () => {
             if (!this.content) {
-                this.content = doXHR();
+                this.content = await doXHR();
                 if (onloaded && this.content) {
                     onloaded(this.content);
                 }
